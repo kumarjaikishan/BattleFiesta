7a8733bd6c4c161ab145f3ad8e5d8be831b7f59c
Jai Kishan <72691369+kumarjaikishan@users.noreply.github.com>
2024-02-10 13:58:33 +0530

    ghgh

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -24,23 +24,50 @@ const Home = () => {
     const dispatch = useDispatch();
     const refer = useRef(null);
     const section3 = useRef(null);
-    const isInview = useInView(refer, { once: false });
-    const isInview1 = useInView(section3, { once: false });
+    const section4 = useRef(null);
+    const section5 = useRef(null);
+    const section6 = useRef(null);
+
+    const isInview = useInView(refer, { once: true });
+    const isInview1 = useInView(section3, { once: true });
+    const isInview2 = useInView(section4, { once: true });
+    const isInview3 = useInView(section5, { once: true });
+    const isInview4 = useInView(section6, { once: true });
     useEffect(() => {
         dispatch(header("Home"))
         dispatch(setloader(false))
     }, [])
     const control = useAnimation();
+    const control1 = useAnimation();
+    const control2 = useAnimation();
+    const control3 = useAnimation();
+    const control4 = useAnimation();
     useEffect(() => {
+        // console.log(isInview);
         if (isInview) {
             control.start('visible');
         }
     }, [isInview])
     useEffect(() => {
         if (isInview1) {
-            control.start('visible');
+            control1.start('visible');
         }
     }, [isInview1])
+    useEffect(() => {
+        if (isInview2) {
+            control2.start('visible');
+        }
+    }, [isInview2])
+    useEffect(() => {
+        if (isInview3) {
+            control3.start('visible');
+        }
+    }, [isInview3])
+    useEffect(() => {
+        if (isInview4) {
+            control4.start('visible');
+        }
+    }, [isInview4])
     const gradientStyle = {
         backgroundImage: 'linear-gradient(to right, #ff5f6d, #ffc371)',
         WebkitBackgroundClip: 'text',
@@ -65,6 +92,22 @@ const Home = () => {
             opacity: 1
         }
     };
+    const leftsection = {
+        hidden: { opacity: 0, scale: .1, x: -200 },
+        visible: {
+            opacity: 1,
+            scale: 1,
+            x: 0
+        }
+    }
+    const rightsection = {
+        hidden: { opacity: 0, scale: .1, x: 200 },
+        visible: {
+            opacity: 1,
+            scale: 1,
+            x: 0
+        }
+    }
 
     return (
         <>
@@ -184,18 +227,25 @@ const Home = () => {
                         </motion.div>
                     </motion.div>
                 </section>
-                <motion.section
-                    ref={section3}
-                    variants={container}
-                    initial="hidden"
-                    animate={control}
-                    className='section3'>
-                    <motion.div variants={item} className="left">
+                <section className='section3'>
+                    <motion.div
+                        ref={section3}
+                        variants={leftsection}
+                        transition={{ duration: .9, delay: .5 }}
+                        initial="hidden"
+                        animate={control1}
+                        className="left">
                         <div className="img">
                             <img src={createform} alt="" />
                         </div>
                     </motion.div>
-                    <motion.div variants={item} className="right">
+                    <motion.div
+                        ref={section3}
+                        variants={rightsection}
+                        transition={{ duration: .9, delay: .5 }}
+                        initial="hidden"
+                        animate={control1}
+                        className="right">
                         <h3>Create and manage tournaments in a go.</h3>
                         <p> <span><img src={tick2} alt="" /></span> <span>Easy creation using a single form.</span></p>
                         <p> <span><img src={tick2} alt="" /></span> <span>Manage multiple matches and teams.</span></p>
@@ -203,28 +253,52 @@ const Home = () => {
                         <p> <span><img src={tick2} alt="" /></span> <span>Add or disqualify team with a single click.</span></p>
                         <p> <span><img src={tick2} alt="" /></span> <span>Manage reports for each and every match.</span></p>
                     </motion.div>
-                </motion.section>
+                </section>
                 <section className='section4'>
-                    <div className="right">
+                    <motion.div
+                        ref={section4}
+                        variants={leftsection}
+                        transition={{ duration: .9, delay: .5 }}
+                        initial="hidden"
+                        animate={control2}
+                        className="right">
                         <h3>Adding results of match made easy</h3>
                         <h4>The process of entering results data of a match has been been super user friendly. User experience has been the top most priority.</h4>
                         <p> <span><img src={tick} alt="" /></span> <span>Add the participating teams once.</span></p>
                         <p> <span><img src={tick} alt="" /></span> <span>While entering results data, just select the team and no. of kills. We take care of the points calculation.</span></p>
                         <p> <span><img src={tick} alt="" /></span> <span>Select a team by just typing its player's name or the team name itself.</span></p>
-                    </div>
-                    <div className="left">
+                    </motion.div>
+                    <motion.div
+                        ref={section4}
+                        variants={rightsection}
+                        transition={{ duration: .9, delay: .5 }}
+                        initial="hidden"
+                        animate={control2}
+                        className="left">
                         <div className="img">
                             <img src={enterresult} alt="" />
                         </div>
-                    </div>
+                    </motion.div>
                 </section>
                 <section className='section5'>
-                    <div className="left">
+                    <motion.div
+                        ref={section5}
+                        variants={leftsection}
+                        transition={{ duration: .9, delay: .5 }}
+                        initial="hidden"
+                        animate={control3}
+                        className="left">
                         <div className="img">
                             <img src={registerform} alt="" />
                         </div>
-                    </div>
-                    <div className="right">
+                    </motion.div>
+                    <motion.div
+                        ref={section5}
+                        variants={rightsection}
+                        transition={{ duration: .9, delay: .5 }}
+                        initial="hidden"
+                        animate={control3}
+                        className="right">
                         <h3>Auto-generated beautiful points table.</h3>
                         <h4>A beautiful points table is generated for your tournament. The standings are calculated using the points system provided during the creation of points table.</h4>
                         <p> <span><img src={tick2} alt="" /></span> <span>A dedicated points table web page is created solely for your tournament.</span></p>
@@ -232,10 +306,16 @@ const Home = () => {
                         <p> <span><img src={tick2} alt="" /></span> <span>Option to export the points table to an image file.</span></p>
                         <p> <span><img src={tick2} alt="" /></span> <span>Detailed analytics.</span></p>
                         <Button variant="contained" size='small'>See Demo</Button>
-                    </div>
+                    </motion.div>
                 </section>
                 <section className='section6'>
-                    <div className="right">
+                    <motion.div
+                        ref={section6}
+                        variants={leftsection}
+                        transition={{ duration: .9, delay: .5 }}
+                        initial="hidden"
+                        animate={control4}
+                        className="right">
                         <h3>Comes with pre-built registration form</h3>
                         <h4>With each eSports tournament you will get a dedicated registration form.</h4>
                         <p> <span><img src={tick} alt="" /></span> <span>Accept team's email, discord id, phone number etc.</span></p>
@@ -243,12 +323,18 @@ const Home = () => {
                         <p> <span><img src={tick} alt="" /></span> <span>Set maximum and minimum player required for registration.</span></p>
                         <p> <span><img src={tick} alt="" /></span> <span>Approve registration or decline. Its up to you.</span></p>
                         <p> <span><img src={tick} alt="" /></span> <span>Teamlist section for participant, to see their Registration Status- Approved, Pending or Rejected</span></p>
-                    </div>
-                    <div className="left">
+                    </motion.div>
+                    <motion.div
+                        ref={section6}
+                        variants={rightsection}
+                        transition={{ duration: .9, delay: .5 }}
+                        initial="hidden"
+                        animate={control4}
+                        className="left">
                         <div className="img">
                             <img src={registerform} alt="" />
                         </div>
-                    </div>
+                    </motion.div>
                 </section>
             </div>
         </>
