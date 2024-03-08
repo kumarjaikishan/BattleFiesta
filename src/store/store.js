e67fddcd9f700bd798efd58078f5625a403c9c23
Jai Kishan <72691369+kumarjaikishan@users.noreply.github.com>
2024-03-08 16:31:49 +0530

    ffd

--- src/store/store.js
+++ src/store/store.js
@@ -8,7 +8,7 @@ import { persistReducer } from 'redux-persist';
 import { combineReducers } from "@reduxjs/toolkit";
 
 const persistConfig = {
-    key: "tournamtntfete",
+    key: "tournamenten",
     version: 1,
     storage: storage
 }
