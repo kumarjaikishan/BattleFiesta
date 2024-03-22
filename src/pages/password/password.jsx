b35dd21ba7b6a099f9ecc23103ef1d6d49a19c36
Jai Kishan <72691369+kumarjaikishan@users.noreply.github.com>
2024-03-22 21:21:59 +0530

    gbfgb

--- src/pages/password/password.jsx
+++ src/pages/password/password.jsx
@@ -4,6 +4,7 @@ import SaveIcon from '@mui/icons-material/Save';
 import LoadingButton from '@mui/lab/LoadingButton';
 import TextField from '@mui/material/TextField';
 import { useState } from 'react';
+import { toast } from "react-toastify";
 
 const PasswordReset = () => {
     const { token } = useParams();
@@ -19,38 +20,41 @@ const PasswordReset = () => {
             ...inp, [naam]: value
         })
     }
-    const handlesubmit =async (e) => {
+    const handlesubmit = async (e) => {
         e.preventDefault();
         console.log(inp);
         try {
-            // setloading(true)
+            setloading(true)
             const rese = await fetch(`${import.meta.env.VITE_API_ADDRESS}setpassword?token=${token}`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
                 },
-                body: JSON.stringify({ password:inp.pass })
+                body: JSON.stringify({ password: inp.pass })
             })
             const resuke = await rese.json();
             console.log(resuke);
-            if (rese.ok) {
-                
+            setloading(false)
+            if (!rese.ok) {
+                return toast.warn(resuke.message, { autoClose: 2100 })
             }
+            toast.success(resuke.message, { autoClose: 1600 })
         } catch (error) {
+            toast.warn(resuke.message, { autoClose: 2100 })
             console.log(error);
+            setloading(false)
         }
     }
-    const [isloading,setloading]= useState(false)
+    const [isloading, setloading] = useState(false)
     return <>
         <div className="passwordpage">
             <div className="box">
                 <h2>Reset Password</h2>
                 <form onSubmit={handlesubmit}>
                     <TextField required name='pass' onChange={handlechange} value={inp.pass} sx={{ width: '90%', mt: 2, mb: 2 }} id="outlined-basic" label="Password" variant="outlined" />
-                    {token}
-                    <TextField required
+                   <TextField required
                         onChange={handlechange}
-                        error={inp.cpass.length ? inp.pass != inp.cpass: false}
+                        error={inp.cpass.length ? inp.pass != inp.cpass : false}
                         helperText="Password must be same" name='cpass' value={inp.cpass} sx={{ width: '90%', mt: 2, mb: 2 }} id="outlined-basic" label="Confirm Password" variant="outlined" />
                     <LoadingButton
                         loading={isloading}
