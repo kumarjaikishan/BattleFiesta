ebce3aed98484c696c8bb8d3bd4283ed4929922f
Jai Kishan <72691369+kumarjaikishan@users.noreply.github.com>
2024-04-06 10:46:17 +0530

    tyhty

--- src/pages/SettingPage/ViewMatches/ViewMatches.jsx
+++ src/pages/SettingPage/ViewMatches/ViewMatches.jsx
@@ -114,8 +114,8 @@ const PointSystem = () => {
       {matches.length < 1 && <div className="notfound">
         <div>
           <SentimentDissatisfiedIcon className="sad" />
-          <h2>No Matches Found for this Tournament</h2>
-          <p>Please Add Matches First.</p>
+          <h2>No Match Found for this Tournament</h2>
+          <p>Please Add Matches First in 'Enter Results' Section.</p>
         </div>
       </div>}
       {matches.map((match, ind) => {
