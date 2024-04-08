62ec3fa7b518996482f63151045de68906e9a532
Jai Kishan <72691369+kumarjaikishan@users.noreply.github.com>
2024-04-08 11:22:53 +0530

    v

--- src/pages/TdmRegistrationPage/teams.jsx
+++ src/pages/TdmRegistrationPage/teams.jsx
@@ -9,19 +9,30 @@ import Badge from '@mui/material/Badge';
 import Button from '@mui/material/Button';
 import group from '../../assets/group2.webp'
 import SentimentVeryDissatisfiedIcon from '@mui/icons-material/SentimentVeryDissatisfied';
+import { useState } from 'react';
 
 const Teams = ({ entry, android, ios }) => {
-
+    const [player, setplayer] = useState(entry);
+    const [active, setactive] = useState(1)
+    const setlist = (no) => {
+        setactive(no)
+        no == 1 && setplayer(entry)
+        no == 2 && setplayer(android)
+        no == 3 && setplayer(ios)
+    }
     return (
         <>
             <div className="teamse">
                 <h2>Player List</h2>
                 <div>
-                    <Badge sx={{m:2}} min={1} badgeContent={android.length} color="success">
-                        <Button  variant="outlined" color="secondary">Android</Button>
+                    <Badge sx={{ m: 2 }} min={1} badgeContent={entry.length} color="success">
+                        <Button onClick={() => setlist(1)} variant={active == 1 ? "contained":'outlined'} color="secondary">All</Button>
                     </Badge>
-                    <Badge sx={{m:2}} min={1} badgeContent={ios.length} color="success">
-                        <Button  variant="outlined" color="secondary">Ios</Button>
+                    <Badge sx={{ m: 2 }} min={1} badgeContent={android.length} color="success">
+                        <Button onClick={() => setlist(2)} variant={active == 2 ? "contained":'outlined'} color="secondary">Android</Button>
+                    </Badge>
+                    <Badge sx={{ m: 2 }} min={1} badgeContent={ios.length} color="success">
+                        <Button onClick={() => setlist(3)} variant={active == 3 ? "contained":'outlined'} color="secondary">Ios</Button>
                     </Badge>
                 </div>
                 {
@@ -33,7 +44,7 @@ const Teams = ({ entry, android, ios }) => {
                         </div>
                     </div>
                 }
-                {entry.map((player, ind) => {
+                {player.map((player, ind) => {
                     return (<Accordion key={ind}
                         style={{ borderRadius: "10px", overflow: "hidden" }}
                         sx={{ mb: 1, minWidth: "95%" }}>
