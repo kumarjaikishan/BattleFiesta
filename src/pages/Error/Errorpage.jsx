05fcf5ee117eba68d984ce80cb1a3a853303e427
Jai Kishan <72691369+kumarjaikishan@users.noreply.github.com>
2024-07-12 13:14:08 +0530

    gdrgrt

--- src/pages/Error/Errorpage.jsx
+++ src/pages/Error/Errorpage.jsx
@@ -19,8 +19,9 @@ export const Errorpage = () => {
     return (
         <div className="errore">
             <div className="container">
-                <h1>404 </h1>
-                <h2>Page Not Found</h2>
+               <div className="img">
+                <img src="https://res.cloudinary.com/dusxlxlvm/image/upload/v1720767933/accusoft/assets/404_page_1_kjlifa.svg" alt="" />
+               </div>
                 <p>Sorry, the page you are looking for does not exist. If you believe there's an issue, feel free to report it, and we'll look into it</p>
                 <div className="btns">
                     <NavLink to="/" className='btn linke'>Return Home</NavLink>
