36e8fd620f9b4801c39d2182e666c2a62ddb68ad
Jai Kishan <72691369+kumarjaikishan@users.noreply.github.com>
2024-09-19 21:57:37 +0530

    rhy

--- dist/assets/dashboard-sMWD2JbP.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./index-6p8941nW.js";const r=()=>a.jsx(a.Fragment,{children:a.jsx("div",{className:"admindashboard",children:a.jsx("h1",{children:"Admin Dashboard"})})});export{r as default};
