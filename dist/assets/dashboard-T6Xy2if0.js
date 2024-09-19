4fba593990a4785f88286a8f7f3aa3ca2e8379d6
Jai Kishan <72691369+kumarjaikishan@users.noreply.github.com>
2024-09-19 22:05:14 +0530

    ftytyr

---
+++ dist/assets/dashboard-T6Xy2if0.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index--P1lYo9Y.js";const r=()=>a.jsx(a.Fragment,{children:a.jsx("div",{className:"admindashboard",children:a.jsx("h1",{children:"Admin Dashboard"})})});export{r as default};
