2f656f5683fa272216c03b33ea49969c2a48dede
Jai Kishan <72691369+kumarjaikishan@users.noreply.github.com>
2024-09-19 21:59:35 +0530

    gfhfh

--- dist/assets/dashboard-Afh3jLoF.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./index-rx_Lnyme.js";const r=()=>a.jsx(a.Fragment,{children:a.jsx("div",{className:"admindashboard",children:a.jsx("h1",{children:"Admin Dashboard"})})});export{r as default};
