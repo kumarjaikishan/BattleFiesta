372960042217a49797dd60b7d3696c2e7b489c40
Jai Kishan <72691369+kumarjaikishan@users.noreply.github.com>
2024-10-17 16:17:58 +0530

    sdefff

--- dist/assets/index-hqqDwnkw.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:dist/assets/index-hqqDwnkw.js
-import{D as H}from"./index-vJf5eBTI.js";function r(t){return H({tag:"svg",attr:{viewBox:"0 0 24 24",fill:"currentColor"},child:[{tag:"path",attr:{d:"M7 4V2H17V4H22V6H20V21C20 21.5523 19.5523 22 19 22H5C4.44772 22 4 21.5523 4 21V6H2V4H7ZM6 6V20H18V6H6ZM9 9H11V17H9V9ZM13 9H15V17H13V9Z"},child:[]}]})(t)}export{r as R};
-========
-import{D as H}from"./index-lmmCRwc6.js";function r(t){return H({tag:"svg",attr:{viewBox:"0 0 24 24",fill:"currentColor"},child:[{tag:"path",attr:{d:"M7 4V2H17V4H22V6H20V21C20 21.5523 19.5523 22 19 22H5C4.44772 22 4 21.5523 4 21V6H2V4H7ZM6 6V20H18V6H6ZM9 9H11V17H9V9ZM13 9H15V17H13V9Z"},child:[]}]})(t)}export{r as R};
->>>>>>>> ce1346603e7afaf8f1373ca251f8c1214de32dd4:dist/assets/index-59yKsXs6.js
