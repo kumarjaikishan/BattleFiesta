2a4ab30cc1b36a4113fe6f5cf08a6b456b535a6e
Jai Kishan <72691369+kumarjaikishan@users.noreply.github.com>
2024-11-03 16:14:34 +0530

    dfg

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -146,10 +146,10 @@ const Dashboard = () => {
     }
   }
   const container = {
-    hidden: { opacity: 1, scale: 0 },
+    // hidden: { opacity: 1, scale: 0 },
     visible: {
-      opacity: 1,
-      scale: 1,
+      // opacity: 1,
+      // scale: 1,
       transition: {
         delayChildren: 0.3,
         staggerChildren: 0.2
