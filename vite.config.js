87c5539136a88f721e5a403384eb8b6b591cbdd0
Jai Kishan <72691369+kumarjaikishan@users.noreply.github.com>
2026-01-18 12:04:49 +0530

    dfdsfdsg

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,32 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import viteCompression from 'vite-plugin-compression';
 import { visualizer } from 'rollup-plugin-visualizer';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     // visualizer({
     //   filename: './stats.html',
     //   open: true,
-    // })
+    // }),
+
+    // Brotli (best)
+    viteCompression({
+      algorithm: 'brotliCompress',
+      ext: '.br',
+      deleteOriginFile: false,
+      filter: (file) => /\.(js|css|html|svg)$/.test(file),
+      threshold: 1024
+    }),
+
+    // Gzip (fallback)
+    viteCompression({
+      algorithm: 'gzip',
+      ext: '.gz',
+      deleteOriginFile: false,
+      filter: (file) => /\.(js|css|html|svg)$/.test(file),
+      threshold: 1024
+    })
   ]
 });
