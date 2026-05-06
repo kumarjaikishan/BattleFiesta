47e65488fbe12ceecb745e8c8b0ffaf1ee6badc0
Jai Kishan <72691369+kumarjaikishan@users.noreply.github.com>
2026-05-06 21:00:22 +0530

    fsdf

--- src/pages/admin/backups/Projects.jsx
+++ src/pages/admin/backups/Projects.jsx
@@ -59,6 +59,7 @@ const Projects = ({ open, onClose }) => {
         { name: 'Portfolio', project: 'portfolio' },
         { name: 'Study Notes', project: 'studynotes' },
         { name: 'Goodnature', project: 'goodnature_ems' },
+        { name: 'RiseOwn', project: 'riseown' },
     ]
 
     return (
